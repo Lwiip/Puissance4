72128474a747d866a9ac306960369f804df7087f
Lwiip <louisgalava@gmail.com>
2016-11-24 10:50:10 +0100

    ajout de insetPion

--- src/Puissance4.java
+++ src/Puissance4.java
@@ -6,7 +6,12 @@ public class Puissance4 {
 	public static void main(String[] args) {
 		Grille grille = new Grille(6,7);
 		grille.affichage();
-
+		grille.insertPion(1, 2);
+		grille.affichage();
+		grille.insertPion(5, 1);
+		grille.affichage();
+		grille.insertPion(5, 2);
+		grille.affichage();
 	}
 
 }
--- src/grille/Grille.java
+++ src/grille/Grille.java
@@ -43,6 +43,18 @@ public class Grille {
 		}
 	}
 	
+	public void insertPion(int y, int idJoueur){ //gerer le cas ou on sort de la grille, et plus rarement pas bon id
+		for(int i = 0; i < this.x; i++){
+			if (this.grille[i][y].getIdJoueur() != 0){ 	//si on on ntrouve un autre pion
+				this.grille[i-1][y].setIdJoueur(idJoueur);
+				return;
+			} else if(i == (this.x - 1)){ //si on est en bout de ligne
+				this.grille[i][y].setIdJoueur(idJoueur);
+				return;
+			}
+		}
+	}
+	
 	
 	/*
 	 * Getter et Setter
