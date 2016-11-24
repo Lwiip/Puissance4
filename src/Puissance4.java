5d77add396c81ccd2b17b21437ab12911b95c34d
Lwiip <louisgalava@gmail.com>
2016-11-24 16:29:03 +0100

    ajout de la detection win

--- src/Puissance4.java
+++ src/Puissance4.java
@@ -5,13 +5,22 @@ public class Puissance4 {
 
 	public static void main(String[] args) {
 		Grille grille = new Grille(6,7);
-		grille.affichage();
+		grille.insertPion(0, 1);
 		grille.insertPion(1, 2);
+		grille.insertPion(1, 1);
+		grille.insertPion(2, 2);
+		grille.insertPion(2, 2);
+		grille.insertPion(2, 1);
+		grille.insertPion(3, 2);
+		grille.insertPion(3, 2);
+		grille.insertPion(3, 2);
+		grille.insertPion(3, 1);
+	
+	
+		
 		grille.affichage();
-		grille.insertPion(5, 1);
-		grille.affichage();
-		grille.insertPion(5, 2);
-		grille.affichage();
+		
+		System.out.println(grille.detectWin(2,3));
 	}
 
 }
