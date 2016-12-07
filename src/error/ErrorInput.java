e95521827d0f02657d46e01b40e5cd638c61fad5
Taknok <pg.developper.fr@gmail.com>
2016-12-07 12:22:32 +0100

    gestion erreurs inscription

--- src/error/ErrorInput.java
+++ src/error/ErrorInput.java
@@ -1,10 +1,7 @@
 package error;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+
+import log.Log;
 
 public class ErrorInput extends RuntimeException{
 	/**
@@ -17,19 +14,7 @@ public class ErrorInput extends RuntimeException{
 	}
 	
 	public ErrorInput(int idJoueur){
-		File saveLog = new File("log.txt");
-		try {
-			if (saveLog.exists() == false) { // si le fichier n'existe pas on le
-												// creer
-				saveLog.createNewFile();
-			}
-			Writer output;
-			output = new BufferedWriter(new FileWriter("log.txt", true)); 
-			output.append("Erreur saisie Joueur " + idJoueur + "\n");
-			output.close();
-		} catch (IOException e) {
-			System.err.println("Erreur saisie Joueur \n");
-		}
+		Log.append("Erreur saisie Joueur " + idJoueur + "\n");
 	}
 	
 }
